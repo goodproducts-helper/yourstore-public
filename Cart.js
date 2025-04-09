7e5ef4353e168cad4eb5c3432e0fe6342f3acc45
goodproducts-helper <kumarbhartiayush@gmail.com>
2025-04-09 20:17:23 +0530

    Create Cart.js

---
+++ Cart.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Cart = ({ cart }) => {
+  return (
+    <div className="cart">
+      <h3>Your Cart</h3>
+      {cart.length === 0 ? (
+        <p>No items in cart</p>
+      ) : (
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index}>
+              {item.name} - ${item.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
